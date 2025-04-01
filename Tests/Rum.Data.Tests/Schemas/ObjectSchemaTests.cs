385973ac48ec675e3bb6cfbf0a83db38bb8cdbcb
Alex Acebo <aacebowork@gmail.com>
2025-04-01 12:25:30 -0700

    Update ObjectSchemaTests.cs

--- Tests/Rum.Data.Tests/Schemas/ObjectSchemaTests.cs
+++ Tests/Rum.Data.Tests/Schemas/ObjectSchemaTests.cs
@@ -34,4 +34,26 @@ public class ObjectSchemaTests(ITestOutputHelper output)
         Assert.Null(res.Error);
         Assert.Equal(value, res.Value);
     }
+
+    [Fact]
+    public void Object_Extend_ShouldError()
+    {
+        var a = Schemas.Object().Property("a", Schemas.String().Enum("hello").Required());
+        var res = a.Validate(new { a = "hello" });
+        Assert.Null(res.Error);
+
+        var b = Schemas.Object().Property("b", Schemas.String().Enum("world").Required());
+        res = b.Validate(new { b = "world" });
+        Assert.Null(res.Error);
+
+        var c = Schemas.Object().Extend(a).Extend(b);
+        res = c.Validate(new { a = "hello" });
+        Assert.NotNull(res.Error);
+
+        res = c.Validate(new { b = "world" });
+        Assert.NotNull(res.Error);
+
+        res = c.Validate(new { a = "hello", b = "world" });
+        Assert.Null(res.Error);
+    }
 }
