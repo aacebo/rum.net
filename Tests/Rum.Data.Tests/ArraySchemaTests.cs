fe4587b144e4273b6595fc01f7caac81e5594a83
Alex Acebo <aacebowork@gmail.com>
2025-04-01 08:20:59 -0700

    Update ArraySchemaTests.cs

--- Tests/Rum.Data.Tests/ArraySchemaTests.cs
+++ Tests/Rum.Data.Tests/ArraySchemaTests.cs
@@ -36,8 +36,32 @@ public class ArraySchemaTests(ITestOutputHelper output)
     [Fact]
     public void Array_SingleItem_ShouldSucceed()
     {
-        var value = new List<object>() { "test" };
+        var value = new List<object>() { "test", "hello" };
         var res = Schemas.Array(Schemas.String().Min(4)).Validate(value);
+
+        if (res.Error != null)
+        {
+            output.WriteLine(res.Error.ToString());
+        }
+
+        Assert.Null(res.Error);
+        Assert.Equal(value, res.Value);
+    }
+
+    [Fact]
+    public void Array_Items_ShouldError()
+    {
+        var schema = Schemas.Array(Schemas.String(), Schemas.Int());
+        var res = schema.Validate(new List<object>() { "a", true });
+        Assert.NotNull(res.Error);
+    }
+
+    [Fact]
+    public void Array_Items_ShouldSucceed()
+    {
+        var value = new List<object>() { "test", 20 };
+        var schema = Schemas.Array(Schemas.String(), Schemas.Int());
+        var res = schema.Validate(value);
 
         if (res.Error != null)
         {
